88a9b7a030ef3875ec40799627da15340fff7617
Sim4n6 <sim4n6@gmail.com>
2020-01-13 20:24:35 +0100

    Added favicon + directions to model + style.css to static

--- recipes_site/content_recipes/models.py
+++ recipes_site/content_recipes/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 class Recipe(models.Model):
     title = models.CharField(max_length=200)
-    ingredients = models.CharField(max_length=600)
-    pub_date = models.DateField('Date published')
+    ingredients = models.TextField(verbose_name="Ingredients", default="Ingredients ... ")
+    directions = models.TextField(verbose_name="Directions", default="Directions ... ")
+    pub_date = models.DateField("Date published")
 
     def __str__(self):
         return self.title
