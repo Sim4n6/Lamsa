960e03adbd88ae933fd435d295db655090e739dc
Sim4n6 <sim4n6@gmail.com>
2020-01-20 22:09:09 +0100

    Mise a jour 2

--- recipes_site/content_recipes/views.py
+++ recipes_site/content_recipes/views.py
@@ -15,4 +15,6 @@ def detail(request, recipe_id):
     recipe = get_object_or_404(Recipe, pk=recipe_id)
     ingredients = Ingredient.objects.filter(recipe__id=recipe_id)
     directions = Direction.objects.filter(recipe__id=recipe_id)
-    return render(request, "content_recipes/detail.html", {"recipe": recipe, "ingredients": ingredients, "directions": directions})
+    latest_recipes = Recipe.objects.order_by('-pub_date')[:4]
+    context = {"recipe": recipe, "ingredients": ingredients, "directions": directions, "latest_recipes": latest_recipes}
+    return render(request, "content_recipes/detail.html", context)
