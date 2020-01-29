96d48f179c37b6ca03f2f05430769a5aa23cae85
Sim4n6 <sim4n6@gmail.com>
2020-01-29 06:19:01 +0100

    deleted load pythondotenv

--- website_project/website/settings.py
+++ website_project/website/settings.py
@@ -11,13 +11,13 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-dotenv_path = os.path.join(BASE_DIR, ".env")
-load_dotenv(verbose=True, dotenv_path=dotenv_path)
+# dotenv_path = os.path.join(BASE_DIR, ".env")
+# load_dotenv(verbose=True, dotenv_path=dotenv_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -148,10 +148,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Media files
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
-
-
-from dotenv import load_dotenv
-load_dotenv(verbose=False)
 
 # CSRF
 CSRF_COOKIE_SECURE = True
