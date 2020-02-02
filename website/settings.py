b1bfa358f751cb28b8b487edbc1d4c5c965b0531
Sim4n6 <sim4n6@gmail.com>
2020-02-02 15:17:14 +0100

    fix settings

--- website/settings.py
+++ website/settings.py
@@ -159,7 +159,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 MEDIA_URL = "/media/"
 
 # # CSRF
